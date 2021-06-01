5960d7a39c1d0ae773492ec8f952ec98bffb6714
Darkdreamnit <49699265+Darkdreamnit@users.noreply.github.com>
2021-05-31 22:18:07 -0700

    Revert "update"
    This reverts commit 59c612564c1914d1c49cfce950fc4583ca609e8e.

--- js/custom.js
+++ js/custom.js
@@ -71,11 +71,11 @@ jQuery(document).ready(function ($) {
 
   //prettyphoto
   $("a[data-pretty^='prettyPhoto']").prettyPhoto({
-    social-tools: ''
+    social_tools: ''
   });
 
   // tooltip
-  $('.social-network li a, .options-box .color a').tooltip();
+  $('.social-network li a, .options_box .color a').tooltip();
 
 
   //scroll to top
